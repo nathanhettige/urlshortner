29ab76b4b9ed2e18c349e9c8dbb083deb3b43953
Nathan Hettige <nathan.hettige@gmail.com>
2022-09-21 00:39:18 +1000

    Mock up a simple page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import PageLayout from './components/PageLayout';
 
 function App() {
+  const Form = () => {
+    return (
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Enter url</span>
+        </label>
+        <label className="input-group">
+          <input
+            type="text"
+            placeholder="http://google.com"
+            className="input input-bordered"
+          />
+          <button className="btn">Shorten!</button>
+        </label>
+      </div>
+    );
+  };
+
   return (
     <>
       <PageLayout>
-        <h1 className="text-4xl text-center">Hello World</h1>
+        <section className="flex flex-col items-center gap-5 h-full">
+          <h1 className="text-4xl">Create Short Links!</h1>
+          <Form />
+        </section>
       </PageLayout>
     </>
   );
